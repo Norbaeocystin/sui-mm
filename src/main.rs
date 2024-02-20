7006acbdae863a488470e096217043ce05faa871
Norbaeocystin <baranrastislav@gmail.com>
2024-02-20 01:56:36 -0300

    working bot

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,25 @@ use std::str::FromStr;
 use log::{debug, LevelFilter};
 use sui_keys::keystore::{AccountKeystore, InMemKeystore};
 use sui_sdk::{SuiClient, SuiClientBuilder};
-use sui_types::base_types::{ObjectID, SuiAddress};
+use sui_types::base_types::{ObjectID, SequenceNumber, SuiAddress};
 use sui_types::crypto::SignatureScheme;
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
-use sui_types::transaction::TransactionKind;
-use sui_types::TypeTag;
+use sui_types::transaction::{ObjectArg, Transaction, TransactionData, TransactionKind};
+use sui_types::{DEEPBOOK_PACKAGE_ID, TypeTag};
 use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
+use serde_json::Value;
+use sui_sdk::rpc_types::{SuiMoveValue, SuiObjectDataOptions, SuiTransactionBlockResponseOptions, SuiTypeTag};
+use sui_types::quorum_driver_types::ExecuteTransactionRequestType;
+use shared_crypto::intent::Intent;
+use sui_sdk::json::SuiJsonValue;
+use sui_sdk::rpc_types::SuiMoveValue::Number;
+use sui_types::object::Owner;
+use sui_types::object::Owner::Shared;
+use sui_types::TypeTag::U64;
 use crate::constant::{SUI_PRICE_FEED, USDC_PRICE_FEED};
 use crate::market::{get_fills, get_market_price};
-use crate::order::list_open_orders;
+use crate::order::{list_open_orders, place_limit_order};
 use crate::pyth::{get_sui_usdc_price, PythFeeder};
 use crate::user::{get_account_balance, get_account_cap, parse_result_account_balance};
 use crate::utils::parse_result_u64;
@@ -43,6 +53,8 @@ async fn main() {
     let response = get_account_cap(&client, &sender ).await.unwrap();
     let object_id = response.data[0].data.clone().unwrap().object_id;
     debug!("{:?}", object_id );
+    let account_cap_raw = client.read_api().get_object_with_options(object_id, SuiObjectDataOptions::new()).await.unwrap();
+    let object_account_cap = account_cap_raw.object().unwrap().object_ref();
     let mut tb = ProgrammableTransactionBuilder::new();
     tb = get_account_balance(tb, sui_tag.clone(), usdc_tag.clone(), pool_id, object_id);
     tb = get_market_price(tb, sui_tag.clone(), usdc_tag.clone(), pool_id);
@@ -58,5 +70,85 @@ async fn main() {
     let result = feeder.get_latest_price().await.unwrap();
     let price = get_sui_usdc_price(result);
     debug!("{:?}", price);
+    let mut tb = ProgrammableTransactionBuilder::new();
+    let result = client.read_api().get_object_with_options(pool_id, SuiObjectDataOptions{
+        show_type: false,
+        show_owner: true,
+        show_previous_transaction: false,
+        show_display: false,
+        show_content: false,
+        show_bcs: false,
+        show_storage_rebate: false,
+    }).await.unwrap();
+    let version = result.object().unwrap().owner.unwrap();
+    match version {
+        Owner::AddressOwner(_) => {
+
+        }
+        Owner::ObjectOwner(_) => {
+
+        }
+        Shared {initial_shared_version} => {
+            debug!("initial shared version: {}", initial_shared_version)
+        }
+        Owner::Immutable => {
+
+        }
+    }
+    println!("version: {:?}", version);
+    let t = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    tb = place_limit_order(tb, sui_tag, usdc_tag, pool_id, SequenceNumber::from_u64(0), 12000000000, 1_400_000, 100_000_000, true, (t + (1000 * 1000)) as u64, 3, object_account_cap);
+    let coins = client
+        .coin_read_api()
+        .get_coins(sender, None, None, None)
+        .await.unwrap();
+    let coin = coins.data.into_iter().next().unwrap();
+    let ptxn = tb.finish();
+    let gas_budget = 50_000_000;
+    // let plo = client.transaction_builder().move_call(sender, DEEPBOOK_PACKAGE_ID,
+    //                                                  "clob_v2", "place_limit_order", vec![
+    //         SuiTypeTag::new("0x2::sui::SUI".parse().unwrap()),
+    //         SuiTypeTag::new("0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN".parse().unwrap()),
+    //     ], vec![
+    //         SuiJsonValue::from_object_id(pool_id),
+    //         SuiJsonValue::new(Number(1).to_json_value()).unwrap(),
+    //         SuiJsonValue::from_str("1500000").unwrap(),
+    //         SuiJsonValue::from_str("1000000000").unwrap(),
+    //         SuiJsonValue::from_str("0").unwrap(),
+    //         SuiJsonValue::from_str("true").unwrap(),
+    //         SuiJsonValue::from_str("1708402699812").unwrap(),
+    //         SuiJsonValue::from_str("0").unwrap(),
+    //         SuiJsonValue::from_object_id(ObjectID::from_str("0x0000000000000000000000000000000000000000000000000000000000000006").unwrap()),
+    //         SuiJsonValue::from_object_id(object_id),
+    //     ], None,
+    //                                                  gas_budget).await.unwrap();
+    let gas_price = client.read_api().get_reference_gas_price().await.unwrap();
+    let tx_data = TransactionData::new_programmable(
+        sender,
+        vec![coin.object_ref()],
+        ptxn,
+        gas_budget,
+        gas_price,
+    );
+    // let tx_data = plo;
+    let signature = keystore.sign_secure(&sender,
+                                         &tx_data,
+                                         Intent::sui_transaction()).unwrap();
+    let tx = Transaction::from_data(tx_data,
+                                    vec![signature],
+    );
+    let t = tx.transaction_data();
+    println!("{:?}", t);
+    let response = client.quorum_driver_api().execute_transaction_block(
+        tx,
+        SuiTransactionBlockResponseOptions::full_content(),
+        Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+
+    ).await;
+    if response.is_err() {
+        println!("got error:{:?}", response)
+    } else {
+        println!("{:?}", response.unwrap().digest);
+    }
 }
 
--- src/order.rs
+++ src/order.rs
@@ -14,20 +14,59 @@ public fun place_limit_order<BaseAsset, QuoteAsset>(
         ctx: &mut TxContext
     ): (u64, u64, bool, u64)
  */
-use sui_types::base_types::ObjectID;
+use std::str::FromStr;
+use sui_types::base_types::{ObjectID, ObjectRef, SequenceNumber};
+use sui_types::digests::ObjectDigest;
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
 use sui_types::transaction::{CallArg, ObjectArg};
 use sui_types::TypeTag;
 use crate::constant::DEEPBOOK_PKG;
 
 // all is in base asset ...
-pub fn place_limit_order(mut tb: ProgrammableTransactionBuilder, baseAsset: TypeTag, quoteAsset: TypeTag) -> ProgrammableTransactionBuilder{
+pub fn place_limit_order(mut tb: ProgrammableTransactionBuilder,
+                         baseAsset: TypeTag,
+                         quoteAsset: TypeTag,
+                         pool_id: ObjectID,
+                         pool_sequence_number: SequenceNumber,
+                         client_order_id: u64,
+                         price: u64,
+                         quantity: u64,
+                         is_bid: bool,
+                         expire_timestamp: u64, // ms
+                         restriction: u8,
+                         account_cap: ObjectRef,
+) -> ProgrammableTransactionBuilder{
+    let pool = ObjectArg::SharedObject{
+        id: pool_id,
+        initial_shared_version: SequenceNumber::from_u64(32079148),
+        mutable: true,
+    };
+    let account_cap = ObjectArg::ImmOrOwnedObject(account_cap);
+    // 0: (account_cap, Default::default(), ObjectDigest::from_str("4KqUgNZCU3fsqeeeShLEHiHtu8bVkgYY7r7wfeq7U157").unwrap()) };
+    let clock_object = ObjectArg::SharedObject {
+        id: ObjectID::from_str("0x0000000000000000000000000000000000000000000000000000000000000006").unwrap(),
+        initial_shared_version: SequenceNumber::from_u64(1),
+        mutable: false
+    };
+    let bid: u8 = if is_bid {1} else {0};
     tb.move_call(
         DEEPBOOK_PKG.parse().unwrap(),
         "clob_v2".parse().unwrap(),
         "place_limit_order".parse().unwrap(),
         vec![baseAsset, quoteAsset],
-        vec![],
+        vec![
+            CallArg::Object(pool), // 1
+            CallArg::Pure(client_order_id.to_le_bytes().to_vec()), // 2
+            CallArg::Pure(price.to_le_bytes().to_vec()), // 3
+            CallArg::Pure(quantity.to_le_bytes().to_vec()), // 4
+            CallArg::Pure(vec![0_u8]), // 5 self matching
+            CallArg::Pure(vec![bid]), // 6
+            CallArg::Pure(expire_timestamp.to_le_bytes().to_vec()), // 7
+            CallArg::Pure(vec![restriction]), // 8
+            CallArg::Object(clock_object), // 9
+            CallArg::Object(account_cap), // 10
+            // CallArg::Object(tx_context_object),
+        ],
     );
     return tb
 }
